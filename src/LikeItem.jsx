da599a3a5bad4a8eb48618aaf76bd765f600b38d
chaerishme <sbk93@naver.com>
2026-06-26 17:34:54 +0900

    feat: 좋아요 보드 기능 완성(phase 1)

---
+++ src/LikeItem.jsx
@@ -0,0 +1,13 @@
+function LikeItem({ item, onLike }) {
+  return (
+    <li className="like-item">
+      <span className="like-item-name">{item.name}</span>
+      <span className="like-item-count">좋아요 {item.likes}</span>
+      <button type="button" className="like-button" onClick={() => onLike(item.id)}>
+        좋아요
+      </button>
+    </li>
+  )
+}
+
+export default LikeItem
